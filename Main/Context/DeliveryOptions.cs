451bdf01031a5a0d743ce431a2559ad344f93448
oleh <olehantemenuk99@outlook.com>
2023-01-29 21:38:43 +0200

    1.0

---
+++ Main/Context/DeliveryOptions.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using WebShop.Main.Conext;
+
+namespace WebShop.Main.Context
+{
+	public class DeliveryOptions
+	{
+        public Guid UserId { get; set; }
+
+        public Guid DeliveryOptionsId { get; set; }
+
+		public string? Country { get; set; }
+
+        public string? City {get;set;}
+
+        public string? Street {get;set;}
+
+        public string? House {get;set;}
+
+        public string? ZipCode {get;set;}
+
+        public User User { get; set; }
+    }
+}
+
